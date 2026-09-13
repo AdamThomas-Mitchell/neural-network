2dec742405c5505e51abf6f0a4d23591edd9718c
AdamThomas-Mitchell <atmitchell017gmail.com>
2026-09-13 16:25:58 +0100

    refactor: update script for data-download call

--- src/neural_network/scripts/data_download.py
+++ src/neural_network/scripts/data_download.py
@@ -1,23 +1,33 @@
 import argparse
+import sys
 from pathlib import Path
 
 from neural_network.data import download_mnist_dataset
+from neural_network.errors import DataError
 
 
 def main() -> None:
     """Main function to download the MNIST dataset."""
 
-    parser = argparse.ArgumentParser(
-        prog="neural_network", description="Download the MNIST dataset"
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "output_dirpath",
+        help="Path to local directory where MNIST files will be downloaded.",
     )
-    parser.add_argument("output_dirpath")
-    parser.add_argument("--overwrite", action="store_true")
+    parser.add_argument(
+        "--overwrite",
+        action="store_true",
+        help="Whether existing MNIST files should be overwritten.",
+    )
     args = parser.parse_args()
 
     output_dirpath: Path = Path(args.output_dirpath)
     overwrite: bool = args.overwrite
 
-    download_mnist_dataset(output_dirpath, overwrite=overwrite)
+    try:
+        download_mnist_dataset(output_dirpath, overwrite=overwrite)
+    except DataError, NotADirectoryError, FileNotFoundError:
+        sys.exit(1)
 
 
 if __name__ == "__main__":
